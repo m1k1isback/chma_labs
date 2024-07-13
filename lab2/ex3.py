0cde65d9d0b25439f5464c71c852fee4a897f2c2
m1k1isback <egorkahockey@gmail.com>
2024-07-14 01:11:34 +0500

    fixed the code ex3.py

--- lab2/ex3.py
+++ lab2/ex3.py
@@ -6,14 +6,13 @@ sigma = 1
 myu = 2
 
 x = np.linspace(-4, 7, 100)
+x1 = np.random.normal(myu, sigma, 100)
 
-f_x = (1 / sigma * np.sqrt(2*pi))*np.exp(-(x-myu)**2/2*sigma**2)
-print(f_x)
+f_x = (1 / (sigma * np.sqrt(2 * pi))) * np.exp(-(x - myu)**2 / (2 * sigma**2))
 
 plt.plot(x, f_x, 'k-', label='f(x)')
 
-# Гистограмма для f_x
-plt.hist(f_x, 20, density=True, facecolor='b')
-plt.axis([-4.0, 8.0, 0.0, 10])
+plt.hist(x1, 20, density=True, facecolor='b')
+plt.axis([-4, 8, 0, 0.5])
 plt.grid()
 plt.show()
