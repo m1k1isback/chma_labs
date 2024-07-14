13fb8d9c362d6505d36cbafbb240351a6ea4f3ce
m1k1isback <egorkahockey@gmail.com>
2024-07-14 23:07:03 +0500

    add ex1.py of lab 3

---
+++ lab3/ex1.py
@@ -0,0 +1,6 @@
+from scipy import constants as cnst
+import scipy
+
+print(f"Boltzman's constant: {cnst.Boltzmann}\n")
+print(f"Planck's constant in eV: {cnst.Planck:.4e} eV\n")
+print(f"Proton mass: {cnst.proton_mass:.4e} kg\n")
